a9f84fd5930f264068e871c8aeef2a91b86992f7
Alejo <alejo.zito@mercadolibre.com>
2024-03-03 21:00:34 -0300

    csv input

--- main.py
+++ main.py
@@ -1,140 +1,174 @@
+import datetime
+from decimal import Decimal
+import unicodedata
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
 
+
+# Por c item recorrer y popular dataframe
+# Volcar todo a un file
+csv_location = "input.csv"
+base_url = ""
+usdQuotation = 1150
+kmsScoringMultiplier = 200000
+
+class BusquedaModelo:
+    def __init__(self, url, transmision, tipoDePublicacion, marca, modelo, version, carroceria):
+        self.Url = url #e.g.: https://autos.mercadolibre.com.ar/fiat/500/manual/fiat-500-sport
+        self.Transmision = transmision
+        self.TipoDePublicacion = tipoDePublicacion
+        self.Marca = marca
+        self.Modelo = modelo
+        self.Version = version
+        self.Carroceria = carroceria
+
+    def __str__(self):
+        return 'url:{}, transmision:{}, tipoDePublicacion:{}, marca:{}, modelo:{}, version:{}, carroceria:{}'.format(
+            self.Url,
+            self.Transmision,
+            self.TipoDePublicacion,
+            self.Marca,
+            self.Modelo,
+            self.Version,
+            self.Carroceria
+        )
+
+class CsvParser():
+
+    def readFile(self, file_location):
+        df = pd.read_csv(file_location)
+        return list(map(lambda x:BusquedaModelo(
+            url=x[0],
+            transmision=x[1],
+            tipoDePublicacion=x[2],
+            marca=x[3],
+            modelo=x[4],
+            version=x[5],
+            carroceria=x[6]
+            ),df.values.tolist()))
+
+    
+
 class Scraper():
 
-    def menu(self):
-        menu = ("""
-    Escoge el país:
-    1. Argentina
-    2. Bolivia
-    3. Brasil
-    4. Chile
-    5. Colombia
-    6. Costa Rica
-    7. Dominicana
-    8. Ecuador
-    9. Guatemala
-    10. Honduras
-    11. México
-    12. Nicaragua
-    13. Panamá
-    14. Paraguay
-    15. Perú
-    16. Salvador
-    17. Uruguay
-    18. Venezuela
-        """)
+    parsed_results = []
+    
+    def process_post(self, post, scrapingInput:BusquedaModelo):
+        # get the title
+        title = post.find('h2').text
+        
+        # CUSTOM
+        currency = post.find('span', class_='andes-money-amount__currency-symbol').text
 
-        valid_options = list(range(1, 19))
+        attributes = post.find_all('li', class_='ui-search-card-attributes__attribute')
+        year = int(attributes[0].text)
+        kms = float(attributes[1].text.replace(".","").replace(" Km", ""))
 
-        while True:
-            print(menu)
-            opcion = int(input('Número de país (Ejemplo: 5): '))
+        city = post.find('span', class_='ui-search-item__group__element ui-search-item__location').text
 
-            if opcion in valid_options:
+        score = round(year + kms / kmsScoringMultiplier, 3)
 
-                urls = {
-                1: 'https://listado.mercadolibre.com.ar/',
-                2: 'https://listado.mercadolibre.com.bo/',
-                3: 'https://listado.mercadolibre.com.br/',
-                4: 'https://listado.mercadolibre.cl/',
-                5: 'https://listado.mercadolibre.com.co/',
-                6: 'https://listado.mercadolibre.com.cr/',
-                7: 'https://listado.mercadolibre.com.do/',
-                8: 'https://listado.mercadolibre.com.ec/',
-                9: 'https://listado.mercadolibre.com.gt/',
-                10: 'https://listado.mercadolibre.com.hn/',
-                11: 'https://listado.mercadolibre.com.mx/',
-                12: 'https://listado.mercadolibre.com.ni/',
-                13: 'https://listado.mercadolibre.com.pa/',
-                14: 'https://listado.mercadolibre.com.py/',
-                15: 'https://listado.mercadolibre.com.pe/',
-                16: 'https://listado.mercadolibre.com.sv/',
-                17: 'https://listado.mercadolibre.com.uy/',
-                18: 'https://listado.mercadolibre.com.ve/',
-                }
+        # get the price
+        if(currency == "U$S"):
+            price = Decimal(post.find('span', class_='andes-money-amount__fraction').text) * 1000
+            usd_price = price
+        else:
+            price = Decimal(post.find('span', class_='andes-money-amount__fraction').text.replace(".",""))
+            usd_price = int((price / usdQuotation))
 
-                self.base_url = urls[opcion]
-                break
-            else:
-                print("Escoge un número del 1 al 18")
+        # get the url post
+        post_link = post.find("a")["href"]
+        # get the url image
+        try:
+            img_link = post.find("img")["data-src"]
+        except:
+            img_link = post.find("img")["src"]
 
+        scraping_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
+        # save in a dictionary
+        post_data = {
+            "title": title,
+            "marca": scrapingInput.Marca,
+            "modelo": scrapingInput.Modelo,
+            "version": scrapingInput.Version,
+            "carroceria": scrapingInput.Carroceria,
+            "tipoDePublicacion": scrapingInput.TipoDePublicacion,
+            "transmision": scrapingInput.Transmision,
+            "price": price,
+            "post link": post_link,
+            "image link": img_link,
+            "currency": currency,
+            "year": year,
+            "kms": kms,
+            "city": city,
+            "usd price": usd_price,
+            "score": score,
+            "scraping_date": scraping_date         
+        }
 
-    def scraping(self):
-        # User search
-        product_name = input("\nProducto: ")
-        # Clean the user input
-        cleaned_name = product_name.replace(" ", "-").lower()
-        # Create the urls to scrap
-        urls = [self.base_url + cleaned_name]
+        if post_data.year > 2024:
+            return
 
-        page_number = 50
-        for i in range(0, 10000, 50):
-            urls.append(f"{self.base_url}{cleaned_name}_Desde_{page_number + 1}_NoIndex_True")
-            page_number += 50
+        # save the dictionaries in a list
+        self.parsed_results.append(post_data)
 
-        # create a list to save the data
-        self.data = []
-        # create counter
-        c = 1
-            
-        # Iterate over each url
-        for i, url in enumerate(urls, start=1):
+    def scraping(self, scrapingInputs):
+        #for each scraping input, generate n urls + dump data into Scraper property
+        for scrapingInput in scrapingInputs:
+            #initialize url with base url
+            urls = list([scrapingInput.Url])
+            print(urls)
 
-            # Get the html of the page
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, 'html.parser')
+            #create other pages urls
+            page_number = 50
+            for i in range(0, 10000, 50):
+                urls.append(f"{scrapingInput.Url}_Desde_{page_number + 1}_NoIndex_True")
+                page_number += 50
+        
+            # Iterate over each url
+            for i, url in enumerate(urls, start=1):
+
+                # Get the html of the first page
+                response = requests.get(url)
+
+                if i == 1:
+                    filenameUrl = url.replace("/", "_").replace(":","").replace(".","")
+                    f = open(r"data/html/" + filenameUrl + ".html", "w", encoding="utf-8")
+                    f.write(response.text)
+                    f.close()
                 
-            # take all posts
-            content = soup.find_all('li', class_='ui-search-layout__item')
-            
-            # Check if there's no content to scrape
-            if not content:
-                print("\nTermino el scraping.")
-                break
-
-            print(f"\nScrapeando pagina numero {i}. {url}")
-            
-            
-            # iteration to scrape posts
-            for post in content:
-                # get the title
-                title = post.find('h2').text
-                # get the price
-                price = post.find('span', class_='andes-money-amount__fraction').text
-                # get the url post
-                post_link = post.find("a")["href"]
-                # get the url image
-                try:
-                    img_link = post.find("img")["data-src"]
-                except:
-                    img_link = post.find("img")["src"]
+                soup = BeautifulSoup(response.text, 'html.parser')
+                    
+                # take all posts
+                content = soup.find_all('li', class_='ui-search-layout__item')
                 
-                # show the data already scraped
-                # print(f"{c}. {title}, {price}, {post_link}, {img_link}")
+                # Check if there's no content to scrape
+                if not content:
+                    print("\nTermino el scraping.")
+                    break
 
-                # save in a dictionary
-                post_data = {
-                    "title": title,
-                    "price": price,
-                    "post link": post_link,
-                    "image link": img_link            
-                }
-                # save the dictionaries in a list
-                self.data.append(post_data)
-                c += 1
+                print(f"\nScrapeando pagina numero {i}. {url}")
+                                    
+                # iteration to scrape posts
+                for post in content:
+                    self.process_post(post, scrapingInput)
 
-    def export_to_csv(self):
+    
+    def export_to_csv(self, filename):
         # export to a csv file
-        df = pd.DataFrame(self.data)
-        df.to_csv(r"data/mercadolibre_scraped_data.csv", sep=";")
+        df = pd.DataFrame(self.parsed_results)
+        filename = filename.replace("/", "_").replace(":","").replace(".","")
+        time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        df.to_csv(r"data/" + filename + time + ".csv", sep=";")
 
 if __name__ == "__main__":
+    csv_parser = CsvParser()
+    scrapingInput = csv_parser.readFile(csv_location)
+
     s = Scraper()
-    s.menu()
-    s.scraping()
-    s.export_to_csv()
+    s.scraping(scrapingInputs=scrapingInput)
+
+    s.export_to_csv("pruebas 3-3-2024")
